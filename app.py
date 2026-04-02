b33b845a0c01c009011a0f35e0a2ef2f8721f40e
Hailey <me@haileyy.dev>
2026-04-02 15:21:26 +0100

    add comments to app.py

--- app.py
+++ app.py
@@ -6,16 +6,19 @@ from src.backend.flask_app import run_flask_server
 from src.backend.smtp_server import run_smtp_server
 
 if __name__ == "__main__":
+    # Check if the script is run as root
     if os.geteuid() != 0:
         print("script must be run as root")
         sys.exit(1)
 
+    # Start the web server and SMTP server in separate threads
     flask_thread = threading.Thread(target=run_flask_server, args=(config.FLASK_HOST, config.FLASK_PORT))
     smtp_thread = threading.Thread(target=run_smtp_server, args=(config.SMTP_HOST, config.SMTP_PORT))
 
     flask_thread.start()
     smtp_thread.start()
 
+    # Run forever until interrupted
     try:
         flask_thread.join()
         smtp_thread.join()
